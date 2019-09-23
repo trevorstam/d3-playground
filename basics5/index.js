3f8b9f1e11d4f0656942be9f0663d9a3034074fe
trevorstam <trevorstam@gmail.com>
2019-09-22 23:49:05 -0700

    add margins and groups

--- basics5/index.js
+++ basics5/index.js
@@ -1,12 +1,36 @@
-const svg = d3.select('svg');
+const svg = d3.select('.canvas')
+  .append('svg')
+  .attr('width', 600)
+  .attr('height', 600);
+
+//create margins and dimensions
+const margin = {
+  top: 20,
+  right: 20,
+  bottom: 100,
+  left: 100
+};
+
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600 - margin.top - margin.bottom;
+
+const graph = svg.append('g')
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 d3.json('menu.json').then(data =>{
 
   //create linear scale
   const y = d3.scaleLinear()
-    .domain([0, 1000])//range of data values in dataset
+    .domain([0, d3.max(data, d => d.orders)])//range of data values in dataset
     .range([0, 500]);//range of linear scale
   
+  // const min = d3.min(data, d => d.orders);
+  // const max = d3.max(data, d => d.orders);
+  // const extent = d3.extent(data, d => d.orders);
+  
+
   const x = d3.scaleBand()
     .domain(data.map(item => item.name))
     .range([0, 500])
@@ -14,7 +38,7 @@ d3.json('menu.json').then(data =>{
     .paddingOuter(0.2); //width is 500px and the width and relative position of the bars is based off of the range
  
   //join data to rects
-  const rects = svg.selectAll('rect')
+  const rects = graph.selectAll('rect')
     .data(data);
 
   rects.attr('width', x.bandwidth)
