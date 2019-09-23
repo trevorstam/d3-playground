0cd0552257062d9576207afc2097795157d060cb
trevorstam <trevorstam@gmail.com>
2019-09-22 23:23:10 -0700

    add linear and band scales

--- basics5/index.js
+++ basics5/index.js
@@ -4,27 +4,28 @@ d3.json('menu.json').then(data =>{
 
   //create linear scale
   const y = d3.scaleLinear()
-    .domain([0, 1000])
-    .range([0, 500]);
+    .domain([0, 1000])//range of data values in dataset
+    .range([0, 500]);//range of linear scale
   
-  console.log(y(400));
-  console.log(y(0));
-  console.log(y(900));
-  
-
+  const x = d3.scaleBand()
+    .domain(data.map(item => item.name))
+    .range([0, 500])
+    .paddingInner(0.2)
+    .paddingOuter(0.2); //width is 500px and the width and relative position of the bars is based off of the range
+ 
   //join data to rects
   const rects = svg.selectAll('rect')
     .data(data);
 
-  rects.attr('width', 50)
+  rects.attr('width', x.bandwidth)
     .attr('height', d => y(d.orders))
     .attr('fill', 'orange')
-    .attr('x', (d, i) => i * 70);
+    .attr('x', d => x(d.name));
 
   rects.enter()
     .append('rect')
-    .attr('width', 50)
+    .attr('width', x.bandwidth)
     .attr('height', d => y(d.orders))
     .attr('fill', 'orange')
-    .attr('x', (d, i) => i * 70);
+    .attr('x', d => x(d.name));
 })
