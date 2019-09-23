6d1565174ea1fe928b12270ac354922424ea7296
trevorstam <trevorstam@gmail.com>
2019-09-22 18:58:18 -0700

    add basic shapes with d3

---
+++ basics2/index.js
@@ -0,0 +1,32 @@
+const canvas = d3.select('.canvas');
+
+// const svg = canvas.append('svg');
+
+// svg.attr('height', 600);
+// svg.attr('width', 600);
+
+//method chaining
+const svg = canvas.append('svg')
+  .attr('height', 600)
+  .attr('width', 600);
+
+//append shapes to svg container
+svg.append('rect')
+  .attr('width', 200)
+  .attr('height', 100)
+  .attr('fill', 'blue')
+  .attr('x', 20)
+  .attr('y', 20);
+
+svg.append('circle')
+  .attr('r', 50)
+  .attr('cx', 300)
+  .attr('cy', 70)
+  .attr('fill', 'pink');
+
+svg.append('line')
+  .attr('x1', 370)
+  .attr('x2', 400)
+  .attr('y1', 20)
+  .attr('y2', 120)
+  .attr('stroke', 'red');
