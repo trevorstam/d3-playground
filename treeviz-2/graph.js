d159a50c0925b504f65a41640a9ae90c1e20fa24
trevorstam <trevorstam@gmail.com>
2019-11-04 23:41:57 -0800

    add dimensions for graph, stratify data for set up of tree data

--- treeviz-2/graph.js
+++ treeviz-2/graph.js
@@ -1,3 +1,39 @@
+const dims = {
+  height: 500,
+  width: 1100
+}; //dimensions for entire tree diagram
+
+const svg = d3.select('.canvas')
+  .append('svg')
+  .attr('width', dims.width + 100)
+  .attr('height', dims.height + 100);
+
+const graph = svg.append('g')
+  .attr('transform', 'translate(50, 50)');
+
+//data stratification
+const stratify = d3.stratify()
+  .id(d => d.name)
+  .parentId(d => d.parent);
+
+const tree = d3.tree()
+  .size([dims.width, dims.height]);
+
+//update function
+const update = (data) =>{
+  //get updated rootnode data
+  const rootNode = stratify(data);
+  
+  const treeData = tree(rootNode);
+  
+  // get nodes selection and join data
+  const nodes = graph.selectAll('.node')
+    .data(treeData.descendants());
+  
+  
+}
+
+//firebase & data
 var data = [];
 
 db.collection('employees').onSnapshot(res =>{
@@ -19,4 +55,6 @@ db.collection('employees').onSnapshot(res =>{
         break;
     }
   });
+
+  update(data);
 });
