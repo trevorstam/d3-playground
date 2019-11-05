d6a999f0368020a204db1a42d5e47d65aadec16f
trevorstam <trevorstam@gmail.com>
2019-11-04 23:05:49 -0800

    hook up form with event listeners to db

--- treeviz-2/index.js
+++ treeviz-2/index.js
@@ -1,2 +1,22 @@
 const modal = document.querySelector('.modal');
 M.Modal.init(modal); //this is syntax from Materialize library
+
+const form = document.querySelector('form');
+const name = document.querySelector('#name');
+const parent = document.querySelector('#parent');
+const department = document.querySelector('#department');
+
+form.addEventListener('submit', event =>{
+  event.preventDefault();
+  db.collection('employees').add({
+    name: name.value,
+    parent: parent.value,
+    department: department.value
+  });
+  //close modal
+  var instance = M.Modal.getInstance(modal);
+  instance.close();
+
+  form.reset();
+
+})
